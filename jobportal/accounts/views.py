398fd687c94469018c8cf7881228933d843ccc99
seandeviniii <shaanrocks1997@gmail.com>
2019-11-25 16:54:11 +0530

    Update views.py

--- jobportal/accounts/views.py
+++ jobportal/accounts/views.py
@@ -13,7 +13,7 @@ def signup(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            email = form.cleaned_data.get('email')
+            username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
             login(request, user)
